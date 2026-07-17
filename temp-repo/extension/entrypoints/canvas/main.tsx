4f1f72930ccf9165b28b8a8740a762e314892af7
刘帅 <2730132473@qq.com>
2026-07-17 18:06:31 +0800

    chore: remove temp-repo directory from tracking and update .gitignore

--- temp-repo/extension/entrypoints/canvas/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import CanvasPage from '../../src/pages/canvas/CanvasPage';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <CanvasPage />
-  </React.StrictMode>,
-);
--- temp-repo/extension/entrypoints/popup/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PopupPage from '../../src/pages/popup/PopupPage';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <PopupPage />
-  </React.StrictMode>,
-);
